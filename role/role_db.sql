a24e283cbdee89426c7c8466e428ae07f9b921e2
Gilberto Hernandez <gilberto.hernandez@snowflake.com>
2026-02-02 14:42:28 -0600

    Formatting

--- role/role_db.sql
+++ role/role_db.sql
@@ -1,31 +1,31 @@
 -- Create a dedicated role for the demo
 
-USE ROLE ACCOUNTADMIN;
+USE ROLE accountadmin;
 
 -- Create the demo role
-CREATE ROLE IF NOT EXISTS AVALANCHE_ADMIN;
+CREATE ROLE IF NOT EXISTS avalanche_admin; 
 
 -- Grant warehouse usage
-GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE AVALANCHE_ADMIN;
+GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE avalanche_admin;
 
 -- Grant ability to create catalog integrations
-GRANT CREATE INTEGRATION ON ACCOUNT TO ROLE AVALANCHE_ADMIN;
+GRANT CREATE INTEGRATION ON ACCOUNT TO ROLE avalanche_admin;
 
 -- Grant ability to create external volumes
-GRANT CREATE EXTERNAL VOLUME ON ACCOUNT TO ROLE AVALANCHE_ADMIN;
+GRANT CREATE EXTERNAL VOLUME ON ACCOUNT TO ROLE avalanche_admin;
 
 -- Grant ability to create databases (for blizzard_data and avalanche_db)
-GRANT CREATE DATABASE ON ACCOUNT TO ROLE AVALANCHE_ADMIN;
+GRANT CREATE DATABASE ON ACCOUNT TO ROLE avalanche_admin;
 
 SET current_user_name = (SELECT CURRENT_USER());
 
 -- Grant the role to the current user
-GRANT ROLE AVALANCHE_ADMIN TO USER IDENTIFIER($current_user_name);
+GRANT ROLE avalanche_admin TO USER IDENTIFIER($current_user_name);
 
 -- Verify grants
-SHOW GRANTS TO ROLE AVALANCHE_ADMIN;
+SHOW GRANTS TO ROLE avalanche_admin;
 
 -- Create database and schema objects ahead of time
-USE ROLE AVALANCHE_ADMIN;
+USE ROLE avalanche_admin;
 CREATE DATABASE IF NOT EXISTS avalanche_db;
 CREATE SCHEMA IF NOT EXISTS avalanche_db.marketing;
